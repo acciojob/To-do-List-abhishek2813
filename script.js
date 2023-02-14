c3063966dbe799686d6cb5348357e329e9742045
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-02-14 13:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+var data ="";
+function addb(){
+	var inputData = document.getElementById("newTodoInput").value;
+    
+	data +=inputData+"<br>";
+	document.getElementById("todoList").innerHTML=data;
+}
+
+
+
 
